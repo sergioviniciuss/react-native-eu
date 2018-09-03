0bf2aa1333e044150b92da5d003ae272878b5f5b
sergioviniciuss <sergioviniciuss@gmail.com>
2018-09-03 09:58:49 +0200

    Project structure

---
+++ core_project/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    console.log('hello world');
+    return (
+      <View style={styles.container}>
+        <View style={{ width:50, height:50, backgroundColor: 'red' }} />
+        <View style={{ width:50, height:50, backgroundColor: 'green' }} />
+        <View style={{ width:50, height:50, backgroundColor: 'blue' }} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flexDirection: 'row', flex: 1, justifyContent: 'center', alignItems: 'center' };
+})
