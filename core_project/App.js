b106989595f23803ead628c9977e6ede35c8c38d
sergioviniciuss <sergioviniciuss@gmail.com>
2018-09-03 11:05:52 +0200

    Exercise 1

--- core_project/App.js
+++ core_project/App.js
@@ -28,10 +28,15 @@ export default class App extends Component {
     console.log('hello world');
     return (
       <View style={styles.container}>
-        <Image
-          source={require('./assets/photo.jpeg')}
-          style={{ height: 100, width: '100%' }}
-        />
+        <View style={[styles.header]} />
+        <View style={[styles.footer]}>
+          <View style={[styles.left]}>
+            <Text style={styles.title}>RN EU</Text>
+          </View>
+          <View style={[styles.right]}>
+            <Text style={styles.desc}>Best RN conference</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -39,5 +44,10 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: { flexDirection: 'column', flex: 1, justifyContent: 'center', alignItems: 'center' },
-  boxStyle: { width: 50, height: 50},
+  header: { flexDirection: 'row', flexGrow: 2, backgroundColor: 'red', width: '100%' },
+  footer: { flexDirection: 'row', flexGrow: 1, backgroundColor: 'white'},
+  left: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  right: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  title: { fontSize: 24, fontWeight: 'bold', },
+  desc: { fontSize: 16 }
 })
